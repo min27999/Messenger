d476baa7f3fe315e4362c86b881e3621a34bc282
min27999 <mine27999@gmail.com>
2022-12-07 00:51:09 +0900

    키워드 알림 기능 추가

--- chat.py
+++ chat.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import *
 from PyQt5 import uic
 from PyQt5.QtCore import pyqtSlot
 from client import Client
+from PyQt5.QtWidgets import QMessageBox
 
 #UI파일 연결
 #단, UI파일은 Python 코드 파일과 같은 디렉토리에 위치해야한다.
@@ -16,11 +17,15 @@ class MainWindow(QMainWindow, form_class) :
 
         self.outputChat.setReadOnly(True)
         self.nameChannel.setReadOnly(True)
+        self.setKeyword.setReadOnly(True)
 
         self.buttonSend.clicked.connect(self.send)
         self.buttonLogin.clicked.connect(self.login)
-        self.buttonSearch.clicked.connect(self.search)
+        self.buttonKeywordSet.clicked.connect(self.keywordSet)
         self.buttonJoin.clicked.connect(self.join)
+        self.buttonKeywordClear.clicked.connect(self.keywordClear)
+
+        self.notifyKeywords = []
 
         self.setWindowTitle("Messenger")
 
@@ -35,11 +40,24 @@ class MainWindow(QMainWindow, form_class) :
 
     def login(self):
         self.nickname = self.inputNickname.text()
+        self.buttonLogin.setDisabled(True)
 
-    def search(self):
-        print("search button")
+    def keywordSet(self):
+        self.notifyKeywords.append(self.inputKeyword.text().strip())
+        self.inputKeyword.setText('')
+
+        keywords = ""
+        for word in self.notifyKeywords:
+            keywords += (word + ' ')
+        
+        self.setKeyword.setText(keywords)
+
+    def keywordClear(self):
+        self.notifyKeywords = []
+        self.setKeyword.setText('')
 
     def join(self):
+        self.buttonJoin.setDisabled(True)
         self.channel = self.inputChannel.text()
         self.nameChannel.setText(f"Channel: {self.channel}")
         self.client = Client(self, self.nickname, self.channel)
@@ -48,5 +66,9 @@ class MainWindow(QMainWindow, form_class) :
 
     @pyqtSlot(str)
     def updateOutputChat(self, chat):
+        self.notifyKeyword = 'test'
+        for keyword in self.notifyKeywords:
+            if keyword in chat:
+                QMessageBox.about(self, "키워드 알림", f"{keyword}가 채팅창에 올라왔습니다.")
         chat = self.outputChat.toPlainText() + chat + '\n'
         self.outputChat.setText(chat)
